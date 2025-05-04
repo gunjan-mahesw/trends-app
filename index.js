81856ce86fadf7c0f8889e91ced0dc5dcdeaa045
gunjan-mahesw <gunjanmaheswari002@gmail.com>
2025-05-04 22:43:21 +0530

    added get

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
